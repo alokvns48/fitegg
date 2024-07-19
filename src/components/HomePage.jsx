fc7e5ecb732dd44bdf5cff712ea98e0499c69889
akshay agnihotri <agnihotriakshay36@gmail.com>
2024-07-20 03:17:57 +0530

    button styling added

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -26,7 +26,7 @@ function HomePage() {
                   maiores iste tenetur culpa cupiditate similique impedit
                   corrupti exercitationem porro? Eligendi, omnis.
                 </p>
-                <button className="tracking-wider font-bigshoulders text-xl text-white mt-12 py-2 w-full bg-[#C39132] shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)]">
+                <button className="focus:outline-none text-lg py-2.5 px-6 rounded-lg bg-gradient-to-b from-[#b5852d] to-[#eabb64] border-2 border-[#323232] border-b-[5px]  transform transition-transform duration-200 linear cursor-pointer active:translate-y-[3px]  tracking-wider font-bigshoulders  text-white mt-12  w-full bg-[#C39132] shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)]">
                   <div className="flex items-center justify-center">
                     <img src={bookicon} alt="" />
                     LEARN MORE
