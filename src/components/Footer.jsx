5d70e37c0dc2e14104e30fe7d7d96d702ce787a0
Alok Pandey <alokpandey@Aloks-Laptop.local>
2024-07-23 22:47:30 +0530

    updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,12 +2,11 @@
 
 function Footer() {
   return (
-    <footer className="footer footer-center bg-[#fff8e3] text-[#3E2F08] p-4">
-      <aside>
+    <footer className="flex justify-center items-center bg-[#fff8e3] text-[#3E2F08] p-4">
         <p>
           Copyright © {new Date().getFullYear()} - All right reserved by FIT EGG
         </p>
-      </aside>
+
     </footer>
   );
 }
