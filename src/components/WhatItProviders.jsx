62a7d8c86f88f99ec5050466b40c8ddd6b8da53f
akshay agnihotri <agnihotriakshay36@gmail.com>
2024-07-23 12:27:16 +0530

    added marquee using gsap

--- src/components/WhatItProviders.jsx
+++ src/components/WhatItProviders.jsx
@@ -1,25 +1,59 @@
-
+import gsap from "gsap";
+import { useEffect } from "react";
 
 function WhatItProviders() {
+
+  useEffect(() => {
+    gsap.fromTo(".marque",{x:"0%"},{
+      x: `${100/3}%`,
+      duration: 14,
+      repeat: -1,
+      ease: "linear",
+    });
+  }, []);
   return (
-    <div>
+    <div className="overflow-hidden">
       <p className="text-[4vw] font-bigshoulders font-bold tracking-wide my-[3vw] text-center text-[#3E2F08]">
         wHAT dOES FITEGG pROVIDES???
       </p>
-      <div className="flex justify-between items-center flex-wrap w-screen h-[150px] border-2 font-slackey text-[1.5vw] text-[#755454] px-[10vw] py-8">
-        <p className="h-full w-2/12 flex items-center ">HIGH PROTIEN</p>
-        <p className="border-l-2 h-full w-2/12 flex items-center pl-6">
-          SOURCE OF OMEGA -3S
-        </p>
-        <p className="border-l-2 h-full w-2/12 flex items-center pl-6">
-          EYE HEALTH
-        </p>
-        <p className="border-l-2 h-full w-2/12 flex items-center pl-6">
-          BUILDS MUSCLES
-        </p>
-        <p className="border-l-2 h-full w-2/12 flex items-center pl-6">
-          CONTRIBUTE TO BONE HEALTH
-        </p>
+      <div className="flex justify-center">
+        <div className="marque flex-shrink-0 flex  justify-between items-center h-[150px] border-2 font-slackey text-[1.5vw] text-[#755454]  py-8 w-[300%]">
+          <p className="h-full flex items-center ">HIGH PROTIEN</p>
+          <p className="border-l-2 h-full  flex items-center pl-6">
+            SOURCE OF OMEGA -3S
+          </p>
+          <p className="border-l-2 h-full flex items-center pl-6">EYE HEALTH</p>
+          <p className="border-l-2 h-full flex items-center pl-6">
+            BUILDS MUSCLES
+          </p>
+          <p className="border-l-2 h-full flex items-center pl-6">
+            CONTRIBUTE TO BONE HEALTH
+          </p>
+
+          <p className="h-full flex items-center ">HIGH PROTIEN</p>
+          <p className="border-l-2 h-full  flex items-center pl-6">
+            SOURCE OF OMEGA -3S
+          </p>
+          <p className="border-l-2 h-full flex items-center pl-6">EYE HEALTH</p>
+          <p className="border-l-2 h-full flex items-center pl-6">
+            BUILDS MUSCLES
+          </p>
+          <p className="border-l-2 h-full flex items-center pl-6">
+            CONTRIBUTE TO BONE HEALTH
+          </p>
+
+          <p className="h-full flex items-center ">HIGH PROTIEN</p>
+          <p className="border-l-2 h-full  flex items-center pl-6">
+            SOURCE OF OMEGA -3S
+          </p>
+          <p className="border-l-2 h-full flex items-center pl-6">EYE HEALTH</p>
+          <p className="border-l-2 h-full flex items-center pl-6">
+            BUILDS MUSCLES
+          </p>
+          <p className="border-l-2 h-full flex items-center pl-6">
+            CONTRIBUTE TO BONE HEALTH
+          </p>
+        </div>
       </div>
     </div>
   );
